95ac8e004ed6c5e7113fc18188f853a87ec3d8a7
hanshengzhao1993 <hanshengzhao1993@gmail.com>
2016-12-15 17:49:30 -0800

    Finished Binary Search Tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,123 @@
 var BinarySearchTree = function(value) {
+  var newBinaryTree = {};
+  newBinaryTree.value = value;
+  newBinaryTree.left = {};
+  newBinaryTree.right = {};
+  _.extend(newBinaryTree, treeMethods1);
+
+  return newBinaryTree;
 };
 
-
+var treeMethods1 = {};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+treeMethods1.insert = function (value) {
+  
+
+  var recurse = function (node) {
+
+
+    if ( node.value > value ) {
+
+      if ( node.left.value === undefined ) {
+        node.left.value = value; 
+        node.left.left = {};
+        node.left.right = {};
+      } else {
+        recurse( node.left );
+      }
+
+    } 
+
+    if ( node.value < value ) {
+      if ( node.right.value === undefined ) {
+        node.right.value = value;
+        node.right.left = {};
+        node.right.right = {};
+      } else {
+        recurse( node.right );
+      }
+    } 
+
+
+  };
+
+  recurse(this);
+
+};
+
+treeMethods1.contains = function (value) {
+  var answer = false;
+
+  var searchNode = function (node) {
+
+    if (node.value === value) {
+      answer = true;
+    }
+
+    if ( node.value < value ) {
+      searchNode(node.right);
+    }
+
+    if ( node.value > value ) {
+      searchNode(node.left);
+    }
+    
+  };
+
+  searchNode(this);
+
+  return answer;
+};
+
+treeMethods1.depthFirstLog = function (func) {
+
+  var searchTree = function (node) {
+
+    node.value = func(node.value);
+
+    if ( node.left.value !== undefined ) {
+      searchTree(node.left);
+    }
+
+    if ( node.right.value !== undefined ) {
+      searchTree(node.right);
+    }
+
+
+
+  };
+
+  searchTree(this);
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,8 +2,8 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree, treeMethods);
+  newTree.children = [];
 
   return newTree;
 };
@@ -11,9 +11,38 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var newChild = {value: value,
+                  children: []};
+
+  _.extend(newChild, treeMethods);
+
+  this.children.push(newChild);
+  // this.children.push({value: value});
+
 };
 
 treeMethods.contains = function(target) {
+  var test = false;
+  if (this.value === target) {
+    return true;
+  }
+
+  var searchChild = function (children) {
+
+    if ( children.length > 0 ) {
+      for (var i = 0; i < children.length; i++) {
+        if ( children[i].value === target) {
+          test = true;
+        }
+        searchChild( children[i].children);
+      }
+    }
+
+  };
+
+  searchChild(this.children);
+
+  return test;
 };
 
 
